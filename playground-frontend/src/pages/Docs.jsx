ac2ba8079f621453d01a84679c05c5f7496f0dd4
Ashish Lal <ashishlal2003@gmail.com>
2024-06-26 19:19:05 +0530

    sidebar

--- playground-frontend/src/pages/Docs.jsx
+++ playground-frontend/src/pages/Docs.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import Sidebar from '../components/docs/Sidebar'
+import React from 'react';
+import Sidebar from '../components/docs/Sidebar';
+import SidebarItem from '../components/docs/SidebarItem';
+import { Home, Bookmark} from 'lucide-react';
 
 function Docs() {
-  return ( 
-    <div className="bg-black text-white w-full h-screen flexpy-4">
-      <Sidebar />
+  return (
+    <div className="bg-black text-white w-full h-screen flex">
+      <Sidebar>
+        <SidebarItem icon={<Home />} text="Home" />
+        <SidebarItem icon={<Bookmark />} text="Bookmark" />
+      </Sidebar>
     </div>
-  )
+  );
 }
 
-export default Docs
+export default Docs;
