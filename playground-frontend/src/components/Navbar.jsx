3598373e915bc905d73134a236c9ac2ba5fea6bb
Nancy <ynancy030@gmail.com>
2024-06-26 17:05:58 +0530

    nav bar

--- playground-frontend/src/components/Navbar.jsx
+++ playground-frontend/src/components/Navbar.jsx
@@ -10,6 +10,7 @@ function Navbar() {
             <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
           </NavLink>
         </div>
+
         <div className="hidden sm:flex sm:items-center sm:justify-center flex-grow">
           <div className="flex space-x-4">
             <NavLink
@@ -50,10 +51,52 @@ function Navbar() {
             </NavLink>
           </div>
         </div>
+
         <div>
-          <NavLink to="/playground" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white border border-white">
+          <NavLink to="/playground" className="hidden sm:block rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white border border-white">
             Get Started
           </NavLink>
+        </div>
+
+        <div className="sm:hidden flex sm:items-center sm:justify-center flex-grow relative">
+          <div className="flex flex-col sm:flex-row space-x-4 bg-white items-start absolute h-screen right-0">
+            <NavLink
+              exact
+              to="/"
+              className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              activeClassName="bg-gray-900 text-white"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/playground"
+              className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              activeClassName="bg-gray-900 text-white"
+            >
+              Playground
+            </NavLink>
+            <NavLink
+              to="/docs"
+              className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              activeClassName="bg-gray-900 text-white"
+            >
+              Docs
+            </NavLink>
+            <NavLink
+              to="/gallery"
+              className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              activeClassName="bg-gray-900 text-white"
+            >
+              Gallery
+            </NavLink>
+            <NavLink
+              to="/community"
+              className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              activeClassName="bg-gray-900 text-white"
+            >
+              Community
+            </NavLink>
+          </div>
         </div>
       </div>
     </nav>
