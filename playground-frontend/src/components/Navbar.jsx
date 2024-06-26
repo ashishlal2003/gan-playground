2b2798df15c932f93cb8be645c981c3bebeffa84
Nancy <ynancy030@gmail.com>
2024-06-26 20:30:46 +0530

    res navbar

--- playground-frontend/src/components/Navbar.jsx
+++ playground-frontend/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import Logo  from '../assets/logo.png'
+import Logo from '../assets/logo.png'
+import ResNavBar from './ResNavBar';
+import { IoMenu } from "react-icons/io5";
 
 function Navbar() {
+  const [isOpen , setIsOpen] = useState(false);
+
   return (
     <nav className="bg-black">
       <div className="flex justify-between items-center py-3 max-w-7xl mx-auto px-4">
         <div className="flex-shrink-0">
           <NavLink to="/">
-            <img className="h-[6rem] w-auto" src={Logo} alt="Your Company" />
+            <img className="sm:h-[6rem] h-[4rem] w-auto" src={Logo} alt="Your Company" />
           </NavLink>
         </div>
 
@@ -58,47 +62,11 @@ function Navbar() {
             Get Started
           </NavLink>
         </div>
-
-        <div className="sm:hidden flex sm:items-center sm:justify-center flex-grow relative">
-          <div className="flex flex-col sm:flex-row space-x-4 bg-white items-start absolute h-screen right-0">
-            <NavLink
-              exact
-              to="/"
-              className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-              activeClassName="bg-gray-900 text-white"
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/playground"
-              className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-              activeClassName="bg-gray-900 text-white"
-            >
-              Playground
-            </NavLink>
-            <NavLink
-              to="/docs"
-              className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-              activeClassName="bg-gray-900 text-white"
-            >
-              Docs
-            </NavLink>
-            <NavLink
-              to="/gallery"
-              className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-              activeClassName="bg-gray-900 text-white"
-            >
-              Gallery
-            </NavLink>
-            <NavLink
-              to="/community"
-              className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-              activeClassName="bg-gray-900 text-white"
-            >
-              Community
-            </NavLink>
-          </div>
-        </div>
+        <IoMenu className='text-white text-4xl absolute z-40 right-4 block sm:hidden' onClick={() => setIsOpen(!isOpen)}/>
+        {
+          isOpen && 
+          <ResNavBar />
+        }
       </div>
     </nav>
   );
